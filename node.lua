f9ee15f79877e49ed78e798b9835decaea66ca10
nullconSharique <167956615+nullconSharique@users.noreply.github.com>
2025-01-07 15:13:09 +0530

    dsadsadsa

--- node.lua
+++ node.lua
@@ -1,4 +1,53 @@
-local font = resource.load_font("assets/roboto.ttf")
+gl.setup(NATIVE_WIDTH, NATIVE_HEIGHT)
+
+local font = resource.load_font "assets/roboto.ttf"
+
+
+local remaining = 0
+local total = 0
+local started
+local x = WIDTH/2
+local size = HEIGHT / 4
+local config
+
+local bar
+
+util.json_watch("config.json", function(new_config)
+    config = new_config
+    size = HEIGHT / config.size
+    bar = resource.create_colored_texture(unpack(config.fg.rgba))
+end)
+
+util.data_mapper{
+  set = function(minutes)
+      remaining = tonumber(minutes) * 60
+      total = remaining
+      started = nil
+  end,
+  play = function()
+      if not started then
+          started = sys.now()
+      end
+  end,
+  pause = function()
+      if started then
+          remaining = remaining - (sys.now() - started)
+          started = nil
+      end
+  end,
+}
+
+local function seconds_remaining()
+  if started then
+      return math.max(0, remaining - (sys.now() - started))
+  else
+      return remaining
+  end
+end
+
+local function clamp(v, min, max)
+  return math.max(min, math.min(max, v))
+end
 
 local schedule = loadScheduleFromCSV() or {
   {"09:00", "Team Meeting"},
@@ -33,10 +82,29 @@ function renderSchedule()
 end
 
 function node.render()
-  gl.clear(0, 0, 0, 1)
-  local currentTime = getCurrentTime()
-  renderClock(currentTime)
-  font:write(500, 500, "Hidsadasdasdasd", 100, 1, 1, 1, 1) 
+  gl.clear(unpack(config.bg.rgba))
+  local text
+  local seconds = seconds_remaining()
+  if seconds <= 0 then
+      text = "Time's up"
+  elseif seconds < 60 then
+      text = string.format("%d s", seconds)
+  elseif seconds % 60 < 1 or seconds > 180 then
+      text = string.format("%d m", seconds/60)
+  else
+      text = string.format("%d m %d s", seconds/60, seconds%60)
+  end
 
-  renderSchedule()   
+  local w = font:width(text, size)
+  local target_x = (WIDTH-w)/2
+  x = x + clamp((target_x-x) / 10, -10, 10)
+  font:write(x, (HEIGHT-size)/2, text, size, unpack(config.fg.rgba))
+  if not started and sys.now()%2 > 1 then
+      local w = font:width("II", size/2)
+      font:write((WIDTH-w)/2, (HEIGHT+size)/2, "II", size/2, unpack(config.fg.rgba))
+  end
+  if total > 0 then
+      local done = 1 - 1 / total * seconds
+      bar:draw(0, HEIGHT - HEIGHT/30, WIDTH * done, HEIGHT, 0.4)
+  end
 end
